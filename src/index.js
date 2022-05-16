5ea3fbc70a19655c49d3bf22aaee91838ed79bf8
Sajjad Zaidi <sajjadzaidi2015@gmail.com>
2022-05-15 22:16:43 -0400

    updating the readme file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const cheerio = require("cheerio");
 const Manager = require("../lib/manager");
 const Engineer = require("../lib/engineer");
 const Intern = require("../lib/intern");
+const generateHtmlString = require('../utils/generateMarkdown');
 
 const question_manager = [
   {
@@ -11,24 +11,53 @@ const question_manager = [
     name: "id",
     message: "Enter the Manager ID",
     //TODO: add validation of ID that ID should > 0
+    validate: function (id) {
+      var num = /^[0-9 ]+$/
+      if (!id.match(num)) {
+        return "Please provide the ID of the Manager";
+      }
+      return true;
+    },
   },
   {
     type: "input",
     name: "name",
     message: "Enter the Manager name",
     //TODO: add validation of ID that ID should > 0
+    validate: function (name) {
+      var pattern = /^[a-zA-Z ]+$/
+      if (!name.match(pattern)) {
+        return "Please provide the name of the Manager";
+      }
+      return true;
+    },
   },
   {
     type: "input",
     name: "email",
     message: "Enter the Manager email",
     //TODO: add validation of ID that ID should > 0
+    validate: function(email)
+    {
+        if(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email)) {
+          return true;
+        }
+        // Regex mail check (return true if valid mail)
+        return 'Please enter a valid email adddress.';
+    }
   },
   {
     type: "input",
     name: "officeNumber",
     message: "Enter the Manager Office Number",
     //TODO: add validation of ID that ID should > 0
+    validate: function (officeNumber) {
+      var num = /^[0-9 ]+$/
+      if (!officeNumber.match(num)) {
+        return "Please provide the ID of the Manager";
+      }
+      return true;
+    },
   },
 ];
 const question_engineer = [
@@ -37,24 +66,52 @@ const question_engineer = [
     name: "id",
     message: "Enter the Engineer ID",
     //TODO: add validation of ID that ID should > 0
+    validate: function (id) {
+      var num = /^[0-9 ]+$/
+      if (!id.match(num)) {
+        return "Please provide the ID of the Engineer";
+      }
+      return true;
+    }
   },
   {
     type: "input",
     name: "name",
     message: "Enter the Engineer name",
     //TODO: add validation of ID that ID should > 0
+    validate: function (name) {
+      var pattern = /^[a-zA-Z ]+$/
+      if (!name.match(pattern)) {
+        return "Please provide the name of the Engineer";
+      }
+      return true;
+    },
   },
   {
     type: "input",
     name: "email",
     message: "Enter the Engineer email",
     //TODO: add validation of ID that ID should > 0
+    validate: function(email)
+    {
+        if(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email)) {
+          return true;
+        }
+        // Regex mail check (return true if valid mail)
+        return 'Please enter a valid email adddress.';
+    }
   },
   {
     type: "input",
     name: "github",
     message: "Enter the Engineer GitHub",
     //TODO: add validation of ID that ID should > 0
+    validate: function (github) {
+      if (github.length > 0) {
+        return true;
+      }
+      return "Please Provide a project title";
+    },
   },
   {
     type: "confirm",
@@ -69,24 +126,53 @@ const question_intern = [
     name: "id",
     message: "Enter the Intern ID",
     //TODO: add validation of ID that ID should > 0
+    validate: function (id) {
+      var num = /^[0-9 ]+$/
+      if (!id.match(num)) {
+        return "Please provide the ID of the Intern";
+      }
+      return true;
+    }
   },
   {
     type: "input",
     name: "name",
     message: "Enter the Intern name",
     //TODO: add validation of ID that ID should > 0
+    validate: function (name) {
+      var pattern = /^[a-zA-Z ]+$/
+      if (!name.match(pattern)) {
+        return "Please provide the name of the Intern";
+      }
+      return true;
+    },
   },
   {
     type: "input",
     name: "email",
     message: "Enter the Intern email",
     //TODO: add validation of ID that ID should > 0
+    validate: function(email)
+    {
+        if(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email)) {
+          return true;
+        }
+        // Regex mail check (return true if valid mail)
+        return 'Please enter a valid email adddress.';
+    }
   },
   {
     type: "input",
     name: "school",
     message: "Enter the Intern School",
     //TODO: add validation of ID that ID should > 0
+    validate: function (name) {
+      var pattern = /^[a-zA-Z ]+$/
+      if (!name.match(pattern)) {
+        return "Please provide the School Name of the Intern";
+      }
+      return true;
+    }
   },
   {
     type: "confirm",
@@ -150,126 +236,8 @@ async function init() {
     );
   });
 }
-// create a body for enigineer
-// append header and body into the div tag
-// append the div tag into the main HTML page
 
-function generateHtmlString(template, data) {
-  const $ = cheerio.load(template.toString());
-  const { manager, engineers, interns } = data;
-  const managerCard = generateManagerCard(manager);
-  const engineerCard = engineers.reduce((acc, curr) => {
-    acc = acc + generateEngineerCard(curr);
-    return acc;
-  }, "");
 
-  const internCard = interns.reduce((acc, curr) => {
-    acc = acc + generateInternCard(curr);
-    return acc;
-  }, "");
-
-  $(".manager").html(managerCard);
-  $(".engineers").html(engineerCard);
-  $(".interns").html(internCard);
-
-  return $.html();
-}
-
-function generateManagerCard(manager) {
-  const managerCardHeaderTemplate = getCardHeaderTemplate(
-    manager.getName(),
-    manager.getRole()
-  );
-  const managerCardBodyTemplate = getBodyTemplateForManager(
-    manager.getId(),
-    manager.getEmail(),
-    manager.getOfficeNumber()
-  );
-  return `
-    <div>
-    ${managerCardHeaderTemplate}
-    ${managerCardBodyTemplate}
-    </div>
-  `;
-}
-
-function generateEngineerCard(engineers) {
-  const engineerCardHeaderTemplate = getCardHeaderTemplate(
-    engineers.getName(),
-    engineers.getRole()
-  );
-  const engineerCardBodyTemplate = getBodyTemplateForEngineer(
-    engineers.getId(),
-    engineers.getEmail(),
-    engineers.getGithub()
-  );
-  return `
-  <div>
-  ${engineerCardHeaderTemplate}
-  ${engineerCardBodyTemplate}
-  </div>
-  `;
-}
-function generateInternCard(interns) {
-  const internCardHeaderTemplate = getCardHeaderTemplate(
-    interns.getName(),
-    interns.getRole()
-  );
-  const internCardBodyTemplate = getBodyTemplateForIntern(
-    interns.getId(),
-    interns.getEmail(),
-    interns.getSchool()
-  );
-  return `
-  <div>
-  ${internCardHeaderTemplate}
-  ${internCardBodyTemplate}
-  </div>
-  `;
-}
-
-function getCardHeaderTemplate(name, role) {
-  return `
-    <div class="card-header">
-      <h3>${name}</h3>
-      <h4>${role}</h4>
-    </div>
-  `;
-}
-
-function getBodyTemplateForManager(id, email, officeNumber) {
-  return `
-    <div class="card-body">
-      <p class="id">ID: ${id}</p>
-      <p class="email">Email: <a href="mailto:${email}">${email}</a></p>
-      <p class="office">Office Number: ${officeNumber}</p>
-    </div>
-  `;
-}
-function getBodyTemplateForEngineer(id, email, github) {
-  return `
-    <div class="card-body">
-      <p class="id">ID: ${id}</p>
-      <p class="email">Email: <a href="mailto:${email}">${email}</a></p>
-      <p class="office">Github: ${github}</p>
-    </div>
-  `;
-}
-function getBodyTemplateForIntern(id, email, school) {
-  return `
-    <div class="card-body">
-      <p class="id">ID: ${id}</p>
-      <p class="email">Email: <a href="mailto:${email}">${email}</a></p>
-      <p class="office">School: ${school}</p>
-    </div>
-  `;
-}
 
 init();
 
-
-// Create template for Engineer and interns
-// move generate temnplate logic to utils like previous assignment
-// turn on the iquirer for the questions and remove hardcoded values
-// fix the css styles on the html and make it match exact
-// add validation on the inquirere
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,97 +1,115 @@
-// function generateManagerCard(manager) {
-//     const managerCardHeaderTemplate = getCardHeaderTemplate(
-//       manager.getName(),
-//       manager.getRole()
-//     );
-//     const managerCardBodyTemplate = getBodyTemplateForManager(
-//       manager.getId(),
-//       manager.getEmail(),
-//       manager.getOfficeNumber()
-//     );
-//     return `
-//       <div>
-//       ${managerCardHeaderTemplate}
-//       ${managerCardBodyTemplate}
-//       </div>
-//     `;
-//   }
-  
-//   function generateEngineerCard(engineers) {
-//     const engineerCardHeaderTemplate = getCardHeaderTemplate(
-//       engineers.getName(),
-//       engineers.getRole()
-//     );
-//     const engineerCardBodyTemplate = getBodyTemplateForEngineer(
-//       engineers.getId(),
-//       engineers.getEmail(),
-//       engineers.getGithub()
-//     );
-//     return `
-//     <div>
-//     ${engineerCardHeaderTemplate}
-//     ${engineerCardBodyTemplate}
-//     </div>
-//     `;
-//   }
-//   function generateInternCard(interns) {
-//     const internCardHeaderTemplate = getCardHeaderTemplate(
-//       interns.getName(),
-//       interns.getRole()
-//     );
-//     const internCardBodyTemplate = getBodyTemplateForIntern(
-//       interns.getId(),
-//       interns.getEmail(),
-//       interns.getSchool()
-//     );
-//     return `
-//     <div>
-//     ${internCardHeaderTemplate}
-//     ${internCardBodyTemplate}
-//     </div>
-//     `;
-//   }
-  
-//   function getCardHeaderTemplate(name, role) {
-//     return `
-//       <div class="card-header">
-//         <h3>${name}</h3>
-//         <h4>${role}</h4>
-//       </div>
-//     `;
-//   }
-  
-//   function getBodyTemplateForManager(id, email, officeNumber) {
-//     return `
-//       <div class="card-body">
-//         <p class="id">ID: ${id}</p>
-//         <p class="email">Email: <a href="mailto:${email}">${email}</a></p>
-//         <p class="office">Office Number: ${officeNumber}</p>
-//       </div>
-//     `;
-//   }
-//   function getBodyTemplateForEngineer(id, email, github) {
-//     return `
-//       <div class="card-body">
-//         <p class="id">ID: ${id}</p>
-//         <p class="email">Email: <a href="mailto:${email}">${email}</a></p>
-//         <p class="office">Github: ${github}</p>
-//       </div>
-//     `;
-//   }
-//   function getBodyTemplateForIntern(id, email, school) {
-//     return `
-//       <div class="card-body">
-//         <p class="id">ID: ${id}</p>
-//         <p class="email">Email: <a href="mailto:${email}">${email}</a></p>
-//         <p class="office">School: ${school}</p>
-//       </div>
-//     `;
-//   }
+const cheerio = require("cheerio");
 
-//   function generateMarkdown(data){
-//     generateManagerCard(data.manager)
-//     generateEngineerCard(data.engineers)
-//     generateInternCard(data.interns)
-//   }
+function generateHtmlString(template, data) {
+  const $ = cheerio.load(template.toString());
+  const { manager, engineers, interns } = data;
+  const managerCard = generateManagerCard(manager);
+  const engineerCard = engineers.reduce((acc, curr) => {
+    acc = acc + generateEngineerCard(curr);
+    return acc;
+  }, "");
 
-//   module.exports = generateMarkdown;
+  const internCard = interns.reduce((acc, curr) => {
+    acc = acc + generateInternCard(curr);
+    return acc;
+  }, "");
+
+  $(".manager").html(managerCard);
+  $(".engineers").html(engineerCard);
+  $(".interns").html(internCard);
+
+  return $.html();
+}
+
+function generateManagerCard(manager) {
+  const managerCardHeaderTemplate = getCardHeaderTemplate(
+    manager.getName(),
+    manager.getRole()
+  );
+  const managerCardBodyTemplate = getBodyTemplateForManager(
+    manager.getId(),
+    manager.getEmail(),
+    manager.getOfficeNumber()
+  );
+  return `
+      <div>
+      ${managerCardHeaderTemplate}
+      ${managerCardBodyTemplate}
+      </div>
+    `;
+}
+
+function generateEngineerCard(engineers) {
+  const engineerCardHeaderTemplate = getCardHeaderTemplate(
+    engineers.getName(),
+    engineers.getRole()
+  );
+  const engineerCardBodyTemplate = getBodyTemplateForEngineer(
+    engineers.getId(),
+    engineers.getEmail(),
+    engineers.getGithub()
+  );
+  return `
+    <div>
+    ${engineerCardHeaderTemplate}
+    ${engineerCardBodyTemplate}
+    </div>
+    `;
+}
+function generateInternCard(interns) {
+  const internCardHeaderTemplate = getCardHeaderTemplate(
+    interns.getName(),
+    interns.getRole()
+  );
+  const internCardBodyTemplate = getBodyTemplateForIntern(
+    interns.getId(),
+    interns.getEmail(),
+    interns.getSchool()
+  );
+  return `
+    <div>
+    ${internCardHeaderTemplate}
+    ${internCardBodyTemplate}
+    </div>
+    `;
+}
+
+function getCardHeaderTemplate(name, role) {
+  return `
+      <div class="card-header">
+        <h3>${name}</h3>
+        <h4>${role}</h4>
+      </div>
+    `;
+}
+
+function getBodyTemplateForManager(id, email, officeNumber) {
+  return `
+      <div class="card-body">
+        <p class="id">ID: ${id}</p>
+        <p class="email">Email: <a href="mailto:${email}">${email}</a></p>
+        <p class="office">Office Number: ${officeNumber}</p>
+      </div>
+    `;
+}
+function getBodyTemplateForEngineer(id, email, github) {
+  return `
+      <div class="card-body">
+        <p class="id">ID: ${id}</p>
+        <p class="email">Email: <a href="mailto:${email}">${email}</a></p>
+        <p class="office">Github: ${github}</p>
+      </div>
+    `;
+}
+function getBodyTemplateForIntern(id, email, school) {
+  return `
+      <div class="card-body">
+        <p class="id">ID: ${id}</p>
+        <p class="email">Email: <a href="mailto:${email}">${email}</a></p>
+        <p class="office">School: ${school}</p>
+      </div>
+    `;
+}
+
+
+module.exports = generateHtmlString;
